b39f0a2af889153f4d8e9a0e29d75f89a12c48c8
I201823049 <i201823049@cibertec.edu.pe>
2019-05-06 18:29:46 -0500

    creacion de formulario(botones y caja de textos)

--- prueba/src/prueba01/Git.java
+++ prueba/src/prueba01/Git.java
@@ -6,10 +6,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class Git extends JFrame {
+public class Git extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JButton btnNewButton;
 
 	/**
 	 * Launch the application.
@@ -35,8 +44,37 @@ public class Git extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(28, 53, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(108, 50, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(this);
+		btnNewButton.setBounds(258, 49, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(28, 102, 328, 122);
+		contentPane.add(scrollPane);
+		
+		JTextArea textArea = new JTextArea();
+		scrollPane.setViewportView(textArea);
 	}
-
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnNewButton) {
+			actionPerformedBtnNewButton(e);
+		}
+	}
+	protected void actionPerformedBtnNewButton(ActionEvent e) {
+		
+		int cant=Integer.parseInt(textField.getText());
+	}
 }
